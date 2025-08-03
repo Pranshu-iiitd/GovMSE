8564b81c55b6486ac5b14080c575041692f8c5ca
Pranshu-iiitd <pranshu23385@iiitd.ac.in>
2025-08-03 17:23:42 +0530

    Ready for Render deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: "/GovConnect/",
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+  },
+  server: {
+    port: 3000,
+  }
 });
